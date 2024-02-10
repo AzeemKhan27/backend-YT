519f6ff3dca0207719e9048fb080e238085416a9
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-02-10 20:17:10 +0530

    add new file for handle async function separately, covered in rapper function

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+
+const asyncHandler = (requestHandler) => {           //using promises
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
