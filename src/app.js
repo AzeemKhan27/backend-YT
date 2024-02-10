2d80e395847715d316cfb6cc247fc7bb91accf42
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-02-10 20:11:02 +0530

    config express in app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';    
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+//-------middlewares-------
+app.use(cors({                        
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}));
+    
+app.use(express.json({limit : "16kb"}));                         // we are allowing JSON data from body/form until 16KB
+app.use(express.urlencoded({extended : true, limit: "16kb"}));   //this middleware allows data from URL.
+app.use(express.static("public"))                                //this middleware allows to store files/images, and giving access to all.
+app.use(cookieParser());                                         //we used cookieParser for set/access cookies from user browser to perform CRUD operation.
+
+
+export { app }
