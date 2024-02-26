5dade48334d717a88406cfeaaf795cced21c6390
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-02-26 17:11:19 +0530

    remove logs from auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,8 +8,6 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
        
         const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
-        console.log("TOKEN :",token)
-
         if(!token){
             throw new ApiError(401,"Unauthorized request")
         }
@@ -19,7 +17,6 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         const user = await User.findById(decodedToken?._id)
                                .select("-password -refreshToken");
         if(!user){
-            // TODO: discuss
             throw new ApiError(401,"Invalid Access Token")
         } 
         
