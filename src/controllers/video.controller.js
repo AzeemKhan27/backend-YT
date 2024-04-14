0c36f84babf4d9b767d8c7cba2056529518c690a
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-04-14 19:57:07 +0530

    get all videos based on query, sort, pagination

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,11 +8,34 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
     //TODO: get all videos based on query, sort, pagination
+    
+    let filter = {};
 
-    const find = await Video.findOne()
-    return res.status(200).json(new ApiResponse(200, find, "retrieved videos."))
+    if(query){
+        filter = { $text: { $search: query }};
+    }
+
+    if(userId){
+        filter.owner = userId;
+    }
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+        lean: true,
+        sort: sortBy ? { [sortBy]: sortType === 'desc' ? -1 : 1 } : { createdAt: -1 },
+    };
+
+    if(!Videos){
+        return res.status(404).json(new ApiResponse(404, {}, 'No videos found.'));
+    }
+
+    // Retrieve paginated videos based on the filter and options
+    const Videos = await Video.aggregatePaginate(filter, options);
+    
+    return res.status(200).json(new ApiResponse(200, Videos, "Retrieved videos."))
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
